2ac913bdbb692356b0ba8c323466e78f52d71dc9
Brian Bleakley <brianbleakley@gmail.com>
2018-08-17 09:10:22 -0600

    add nested collection assertions (#2)

--- assertions/assertions.go
+++ assertions/assertions.go
@@ -1,10 +1,129 @@
 package assertions
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 
 	"github.com/smartystreets/goconvey/convey"
 )
+
+type Assertion = func(actual interface{}, expected ...interface{}) string
+
+// TODO: Support other collection types
+func countPassingElements(assertion Assertion, collection interface{}, expected ...interface{}) (length int, passingCounter int, failures []string, err error) {
+	failures = []string{}
+	switch reflect.TypeOf(collection).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(collection)
+		length = s.Len()
+		for i := 0; i < length; i++ {
+			each := s.Index(i)
+			message := assertion(each.Interface(), expected...)
+			if message == "" {
+				passingCounter++
+			} else {
+				// goconvey does not consistently structure failure message strings, some look like this:
+				// Expected '2' to be less than '2' (but it wasn't)!
+				// others look like this:
+				// {"Message":"Expected: '5'\nActual:   '1'\n(Should be equal)","Expected":"5","Actual":"1"}
+				type Failure struct {
+					Message  string
+					Expected string
+					Actual   string
+				}
+				var f Failure
+				err := json.Unmarshal([]byte(message), &f)
+				if err == nil {
+					failures = append(failures, f.Message)
+				} else {
+					failures = append(failures, message)
+				}
+
+			}
+		}
+	default:
+		return 0, 0, []string{}, fmt.Errorf("The collection is not a valid slice.")
+	}
+	return length, passingCounter, failures, nil
+}
+
+func pluralize(num int, word string) string {
+	if num == 1 {
+		return word
+	}
+	return fmt.Sprintf("%ss", word)
+}
+
+func AtLeast(required int, subAssertion Assertion) Assertion {
+	return func(collection interface{}, expected ...interface{}) string {
+		total, passing, failures, err := countPassingElements(subAssertion, collection, expected...)
+		if err != nil {
+			return err.Error()
+		}
+		if passing < required {
+			combinedMessage := fmt.Sprintf("Expected the collection (length %d) to contain at least %d passing %s, but it contained %d.", total, required, pluralize(required, "element"), passing)
+			combinedMessage = combinedMessage + "\nFailures:\n"
+			for _, message := range failures {
+				combinedMessage = combinedMessage + "\n" + message
+			}
+			return combinedMessage
+		}
+		return ""
+	}
+}
+
+func AtMost(required int, subAssertion Assertion) Assertion {
+	return func(collection interface{}, expected ...interface{}) string {
+		total, passing, _, err := countPassingElements(subAssertion, collection, expected...)
+		if err != nil {
+			return err.Error()
+		}
+		if passing > required {
+			return fmt.Sprintf("Expected the collection (length %d) to contain at most %d passing %s, but it contained %d.", total, required, pluralize(required, "element"), passing)
+		}
+		return ""
+	}
+}
+
+func exactly(required int, all bool, subAssertion Assertion) Assertion {
+	return func(collection interface{}, expected ...interface{}) string {
+		total, passing, failures, err := countPassingElements(subAssertion, collection, expected...)
+		if err != nil {
+			return err.Error()
+		}
+		if all {
+			required = total
+		}
+		if passing != required {
+			combinedMessage := fmt.Sprintf("Expected the collection (length %d) to contain exactly %d passing %s, but it contained %d.", total, required, pluralize(required, "element"), passing)
+			if passing < required {
+				combinedMessage = combinedMessage + "\nFailures:\n"
+				for _, message := range failures {
+					combinedMessage = combinedMessage + "\n" + message
+				}
+			}
+			return combinedMessage
+		}
+		return ""
+	}
+}
+
+func Any(subAssertion Assertion) Assertion {
+	return AtLeast(1, subAssertion)
+}
+
+func All(subAssertion Assertion) Assertion {
+	return exactly(0, true, subAssertion)
+}
+
+func Exactly(required int, subAssertion Assertion) Assertion {
+	return exactly(required, false, subAssertion)
+}
+
+func None(subAssertion Assertion) Assertion {
+	return Exactly(0, subAssertion)
+}
 
 // ShouldHaveErrorMessageWithSubstring takes an error and a message
 // and checks that the error is not nil and contains the message.
--- assertions/assertions_test.go
+++ assertions/assertions_test.go
@@ -8,6 +8,106 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func TestAtLeast(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that at least N elements pass an assertion", func() {
+			So(testSlice, AtLeast(0, ShouldBeGreaterThan), 2)
+			So(testSlice, AtLeast(1, ShouldBeGreaterThan), 2)
+			So(testSlice, AtLeast(2, ShouldBeGreaterThan), 2)
+			failureMessage := AtLeast(3, ShouldBeGreaterThan)(testSlice, 2)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain at least 3 passing elements, but it contained 2.\nFailures:\n\nExpected '1' to be greater than '2' (but it wasn't)!\nExpected '2' to be greater than '2' (but it wasn't)!")
+			failureMessage = AtLeast(1, ShouldBeGreaterThan)(testSlice, 5)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain at least 1 passing element, but it contained 0.\nFailures:\n\nExpected '1' to be greater than '5' (but it wasn't)!\nExpected '2' to be greater than '5' (but it wasn't)!\nExpected '3' to be greater than '5' (but it wasn't)!\nExpected '4' to be greater than '5' (but it wasn't)!")
+		})
+	})
+}
+
+func TestAtMost(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that at most N elements pass an assertion", func() {
+			So(testSlice, AtMost(4, ShouldBeGreaterThan), 2)
+			So(testSlice, AtMost(3, ShouldBeGreaterThan), 2)
+			So(testSlice, AtMost(2, ShouldBeGreaterThan), 2)
+			failureMessage := AtMost(1, ShouldBeGreaterThan)(testSlice, 2)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain at most 1 passing element, but it contained 2.")
+			failureMessage = AtMost(0, ShouldBeGreaterThan)(testSlice, 2)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain at most 0 passing elements, but it contained 2.")
+		})
+	})
+}
+
+func TestExactly(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that exactly N elements pass an assertion", func() {
+			So(testSlice, Exactly(2, ShouldBeLessThan), 3)
+			So(testSlice, Exactly(1, ShouldBeLessThan), 2)
+			So(testSlice, Exactly(0, ShouldBeGreaterThan), 4)
+			failureMessage := Exactly(2, ShouldBeLessThan)(testSlice, 2)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain exactly 2 passing elements, but it contained 1.\nFailures:\n\nExpected '2' to be less than '2' (but it wasn't)!\nExpected '3' to be less than '2' (but it wasn't)!\nExpected '4' to be less than '2' (but it wasn't)!")
+			failureMessage = Exactly(2, ShouldBeLessThan)(testSlice, 4)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain exactly 2 passing elements, but it contained 3.")
+		})
+	})
+}
+
+func TestAny(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that at least 1 element passes an assertion", func() {
+			So(testSlice, Any(ShouldEqual), 3)
+			So(testSlice, Any(ShouldBeLessThan), 3)
+			failureMessage := Any(ShouldEqual)(testSlice, 5)
+			expectedMessage := `Expected the collection (length 4) to contain at least 1 passing element, but it contained 0.
+Failures:
+
+Expected: '5'
+Actual:   '1'
+(Should be equal)
+Expected: '5'
+Actual:   '2'
+(Should be equal)
+Expected: '5'
+Actual:   '3'
+(Should be equal)
+Expected: '5'
+Actual:   '4'
+(Should be equal)`
+			So(failureMessage, ShouldEqual, expectedMessage)
+		})
+	})
+}
+
+func TestAll(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that all elements pass an assertion", func() {
+			So(testSlice, All(ShouldBeGreaterThan), 0)
+			failureMessage := All(ShouldBeGreaterThan)(testSlice, 1)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain exactly 4 passing elements, but it contained 3.\nFailures:\n\nExpected '1' to be greater than '1' (but it wasn't)!")
+		})
+	})
+}
+
+func TestNone(t *testing.T) {
+	Convey("Given a slice", t, func() {
+		testSlice := []int{1, 2, 3, 4}
+
+		Convey("It ensures that no elements pass an assertion", func() {
+			So(testSlice, None(ShouldEqual), 5)
+			failureMessage := None(ShouldEqual)(testSlice, 4)
+			So(failureMessage, ShouldEqual, "Expected the collection (length 4) to contain exactly 0 passing elements, but it contained 1.")
+		})
+	})
+}
+
 func TestShouldHaveErrorMessageWithSubstring(t *testing.T) {
 	Convey("Given actual and expected", t, func() {
 		Convey("If expected has more than 1 argument, an message should be returned", func() {
